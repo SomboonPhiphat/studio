3042f889026850a11d03572d4c8da885770b7ef7
Somboon Phiphatcharoenwong <somboon.phiphatcharoenwong@g.swu.ac.th>
2026-02-16 14:21:59 +0000

    คำว่า Calculator เล็กลงครึ่งหนึ่ง  (_for element h1_)

--- src/components/sawasdee-health/Header.tsx
+++ src/components/sawasdee-health/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
           <ModeToggle />
         </div>
         <h1 className="pt-8 text-4xl font-bold text-gradient bg-gradient-to-r from-primary to-blue-400 dark:to-cyan-400 md:text-5xl">
-          Thai EQ-5D-5L Calculator
+          Thai EQ-5D-5L <span className="text-lg md:text-2xl">Calculator</span>
         </h1>
         <p className="mt-2 text-lg text-muted-foreground">
           เครื่องมือประเมินคุณภาพชีวิตและค่าอรรถประโยชน์
